b57413a4bcb7d07a521b268ad0b7121482a1e211
miugurlu <miugurlu@icloud.com>
2024-05-18 01:33:39 +0300

    giriş arayüzü ve müşteri temsilcisi arayüzü

---
+++ bonus_objection_list.py
@@ -0,0 +1,7 @@
+# bonus_objection_list.py
+
+import tkinter as tk
+
+def show_bonus_objection_list():
+    # İlgili işlevselliği gerçekleştirecek kod buraya gelecek
+    pass
---
+++ customer_call_list.py
@@ -0,0 +1,7 @@
+# customer_call_list.py
+
+import tkinter as tk
+
+def show_customer_call_list():
+    # İlgili işlevselliği gerçekleştirecek kod buraya gelecek
+    pass
